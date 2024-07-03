04ab7225528bec14a1a2e22146deb3f7121741e5
Daniel Feller <70229820+danFeller@users.noreply.github.com>
2024-07-03 20:05:08 +0000

    Finished

--- app.rb
+++ app.rb
@@ -2,8 +2,33 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_with_form)
+end
+
+get("/square/new") do
+  erb(:square_with_form)
+end
+get("/square/results") do
+  erb(:square_with_form_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_with_form)
+end
+get("/square_root/results") do
+  erb(:square_root_with_form_results)
+end
+
+get("/random/new") do
+  erb(:random_with_form)
+end
+get("/random/results") do
+  erb(:random_with_form_results)
+end
+
+get("/payment/new") do
+  erb(:payment_with_form)
+end
+get("/payment/results") do
+  erb(:payment_with_form_results)
 end
